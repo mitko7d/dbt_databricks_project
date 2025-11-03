a8447abb7c0c721362f5a1fb5937be0a29f78051
mitko7d@gmail.com <mitko7d@gmail.com>
2025-11-03 14:49:47 +0000

    added macro to multiply columns

--- models/silver/silver_orders.sql
+++ models/silver/silver_orders.sql
@@ -5,5 +5,5 @@ select
     product_id,
     quantity,
     unit_price,
-    quantity * unit_price as order_amount
+    {{ multiply_columns_and_round('quantity', 'unit_price') }} as order_amount
 from {{ref("bronze_orders")}}
